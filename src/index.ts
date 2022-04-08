ca22884e4cc9436e6320e9d50d96a3bc1087fd6b
roylin <1002591652@qq.com>
2022-04-08 16:54:35 +0800

    docs: init

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,3 @@ export function factory(): BaseBackend {
       throw new Error(`OS "${platform}" is not supported.`);
   }
 }
-
-if (process.env.NODE_ENV === "development") {
-  async function main() {
-    const engine = await factory();
-    // await engine.init();
-    // await engine.start();
-
-    const IMAGE = "hello-world";
-
-    await engine.pullImage(IMAGE);
-
-    const id = (await engine.run(IMAGE, {
-      debug: true,
-      detach: true,
-    })) as string;
-    console.log(id);
-    const images = await engine.getImages();
-    console.log(images);
-    console.log(await (await engine.rm(id)).stdout);
-  }
-  main();
-}
