ccf9bfd9fb483764a8f36c85c1838620b0dc40cc
roylin <1002591652@qq.com>
2022-04-09 18:56:52 +0800

    feat: added RunCommandFlags properties

--- src/types/container.ts
+++ src/types/container.ts
@@ -1,7 +1,60 @@
 import { GlobalFlags } from "./global";
 
 export interface RunCommandFlags extends GlobalFlags {
+  addHost?: string[];
+  blkioWeight?: number;
+  capAdd?: string[];
+  capDrop?: string[];
+  cgroupConf?: string[];
+  cgroupns?: string;
+  cidfile?: string;
+  cosignKey?: string;
+  cpuPeriod?: number;
+  cpuQuota?: number;
+  cpuShares?: number;
+  cpus?: number;
+  cpusetCpus?: string;
+  cpusetMems?: string;
   detach?: boolean;
+  device?: string[];
+  dns?: string[];
+  entrypoint?: string;
+  env?: string[];
+  envFile?: string[];
+  gpus?: string[];
+  hostname?: string;
+  interactive?: boolean;
+  ip?: string;
+  label?: string[];
+  labelFile?: string[];
+  memory?: string;
+  mount?: string[];
+  name?: string;
+  net?: string[];
+  network?: string[];
+  pid?: string;
+  pidfile?: string;
+  pidsLimit?: number;
+  platform?: string;
+  privileged?: boolean;
+  publish?: string[];
+  pull?: string;
+  rdtClass?: string;
+  readOnly?: boolean;
+  restart?: string;
+  rm?: boolean;
+  rootfs?: boolean;
+  runtime?: string;
+  securityOpt?: string[];
+  shmSize?: string;
+  sysctl?: string[];
+  tmpfs?: string[];
+  tty?: boolean;
+  ulimit?: string[];
+  user?: string;
+  verify?: string;
+  volume?: string[];
+  workdir?: string;
 }
 
 export interface ContainerCommandFlags extends GlobalFlags {}
